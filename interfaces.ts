531f840cbc56c40542ba2c1f66b24b0603da9d12
Diego Donis <diego.jdonis@gmail.com>
2022-05-12 12:44:45 -0600

    version 1.0

---
+++ interfaces.ts
@@ -0,0 +1,5 @@
+export interface ifiles {
+  nombre: string
+  extension: string
+}
+export type extensiones = "exe" | "txt" | "pdf" | "epub" | "jpg" | "jpeg" | "png" | "raw" | "mp3" | "mp4" | "mkv" | "xlsx" | "doc" | "rar" | "zip";
---
+++ ordenar.ts
@@ -0,0 +1,26 @@
+import { ensureDirSync, moveSync } from "https://deno.land/std@0.139.0/fs/mod.ts";
+import { ifiles, extensiones } from "./interfaces.ts";
+import { folders } from "./admitidos.ts";
+
+let path: string = prompt('ingresa el directorio que deseas ordenar') || '';
+if (path[path.length - 1] != "\\" || path[path.length - 1] != "/") path = path + '/'
+
+let { archivos, carpetas }: { archivos: ifiles[], carpetas: string[] } = { archivos: [], carpetas: [] }
+
+for await (const dirEntry of Deno.readDir(path)) {
+  if (dirEntry.isFile) {
+    const sn = dirEntry.name.split('.')
+    const extension = sn[sn.length - 1]
+    archivos.push({ nombre: dirEntry.name, extension })
+  } else if (dirEntry.isDirectory) {
+    carpetas.push(dirEntry.name)
+  }
+}
+for await (const file of archivos) {
+  if (Object.keys(folders).includes(file.extension)) {
+    const extension = file.extension as extensiones
+    await ensureDirSync(path + folders[extension])
+    moveSync(`${path}${file.nombre}`, `${path}${folders[extension]}/${file.nombre}`)
+  }
+}
+console.log('termino')
